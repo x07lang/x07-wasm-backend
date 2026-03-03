69feab1cf790168fb6dbbc5614c9eb03335dd108
Bodik <webodik@gmail.com>
2026-03-02 17:04:43 -0800

    chore(fmt): cargo fmt

--- crates/x07-wasm/src/device/host_abi.rs
+++ crates/x07-wasm/src/device/host_abi.rs
@@ -3,4 +3,3 @@ pub(crate) const ABI_NAME: &str = "webview_host_v1";
 pub(crate) const ABI_VERSION: &str = "0.1.0";
 pub(crate) const HOST_ABI_HASH_HEX: &str =
     "17f08717dab342dadb8f366631eb5d2fd7e3a877e86a3c705a533fc073eb9740";
-
